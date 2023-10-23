7e111f1dd1f977532e1ea12b8bceb45aa0e17cfc
Bowie <ailtsantz@gmail.com>
2023-10-23 17:12:43 -0300

    My-test

--- App.js
+++ App.js
@@ -1,20 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity  } from 'react-native';
+import React, {useState} from 'react'
+import axios from 'axios'
 
 export default function App() {
+  const [infoCep, setInfoCep] = useState({})
+    const [searchCep, setSearchCep] = useState('')
+
+    const getCep = async() =>{
+        const {data} = await axios.get(`http://viacep.com.br/ws/${searchCep}/json/`)
+        setInfoCep(data)
+        console.log(data)
+        console.log('CEP pesquiado: '+searchCep)
+    }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <View style={{flex:1,alignItems:'center', marginTop:35}}>
+      <TextInput 
+          placeholder="Digite o CEP" 
+          value={searchCep}
+          keyboardType='numeric'
+          onChangeText={text => setSearchCep(text)}
+          style={{backgroundColor:'#F5F7F8',width:'90%',height:42,borderRadius:5,alignItems:'center',justifyContent:'center',paddingLeft:10,paddingRight:10}}
+      />
+      <TouchableOpacity style={{width:'90%',height:42,borderRadius:5,marginTop:25,alignItems:'center',justifyContent:'center',backgroundColor:'#0A4D68'}}
+          onPress={getCep}
+      >
+          <Text style={{color:'white',fontSize:20,fontWeight:500}}>
+              Buscar
+          </Text>
+      </TouchableOpacity>
+      <View style={{flex:1, marginTop: 20,width:'90%'}}>
+          <Text>Rua: {infoCep.logradouro}</Text>
+          <Text>Bairro: {infoCep.bairro}</Text>
+          <Text>Cidade: {infoCep.localidade}</Text>
+          <Text>Estado: {infoCep.uf}</Text>
+      </View>
+  </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
